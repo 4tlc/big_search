21c85f8bafc77263f0d6a822a4888cd59bfa4aa7
Timothy Cronin <tcroniniv@gmail.com>
2023-01-22 20:08:56 -0500

    read files into a vector

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::fs;
 use std::fs::{DirEntry, ReadDir};
 
-static mut FILES: Vec<DirEntry> = Vec::<DirEntry>::new();
-
 fn main() {
+    let mut files: Vec<DirEntry> = Vec::<DirEntry>::new();
     let paths: ReadDir = fs::read_dir("./").unwrap();
-    get_files(paths);
+    get_files_old(&mut files, paths);
+    println!("{}", files.len());
+    let mut files: Vec<DirEntry> = Vec::<DirEntry>::new();
+    let paths: ReadDir = fs::read_dir("./").unwrap();
+    get_files_new(&mut files, paths);
+    println!("{}", files.len());
     // let paths = fs::read_dir("./").unwrap();
 
     // for path in paths {
@@ -13,11 +17,32 @@ fn main() {
     // }
 }
 
-fn get_files(paths: ReadDir) -> () {
+fn get_files_old(files: &mut Vec<DirEntry>, paths: ReadDir) -> () {
     for path in paths {
         match fs::read_dir(path.as_ref().unwrap().path()) {
-            Ok(_) => get_files(fs::read_dir(path.unwrap().path().display().to_string()).unwrap()),
-            Err(_) => println!("file: {}", path.unwrap().path().display()),
+            Ok(_) => {
+                get_files_old(
+                    files,
+                    fs::read_dir(path.unwrap().path().display().to_string()).unwrap(),
+                );
+            }
+            Err(_) => {
+                files.push(path.unwrap());
+            }
+        }
+    }
+}
+
+fn get_files_new(files: &mut Vec<DirEntry>, paths: ReadDir) -> () {
+    for path in paths {
+        let p = fs::read_dir(path.as_ref().unwrap().path());
+        match p {
+            Ok(_) => {
+                get_files_new(files, p.unwrap());
+            }
+            Err(_) => {
+                files.push(path.unwrap());
+            }
         }
     }
 }
