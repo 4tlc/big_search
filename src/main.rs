adc7bb8a94177f1b3cab827ff265332aca830808
Timothy Cronin <tcroniniv@gmail.com>
2023-01-24 19:49:46 -0500

    clean up

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,30 @@
 mod search_files;
-use crate::search_files::{loop_files, search_file};
+use search_files::{loop_files, search_file};
 mod parse_args;
-use crate::parse_args::parse_args;
+use parse_args::parse_args;
+mod file_size;
+use file_size::search_size;
 use std::env;
-use std::fs;
-use std::io;
 use std::path::PathBuf;
 
 pub static mut MATCHED_FILES: Vec<String> = Vec::<String>::new();
 pub static mut SEARCHED_SIZE: u64 = 0;
 
 fn main() {
-    let (path, target) = parse_args(env::args().collect());
-    let p = PathBuf::new();
+    let (path, target): (PathBuf, String) = parse_args(env::args().collect());
+    println!("Size: {}", search_size(&path));
     begin_status();
-    let mut size: u64 = 0;
-    size = search_size(PathBuf::from(&path));
-    match std::fs::read_dir(PathBuf::from(&path)) {
+    match std::fs::read_dir(&path) {
         Ok(dir) => {
             loop_files(&target, dir);
         }
         Err(_) => {
-            search_file(&target, PathBuf::from(path));
+            search_file(&target, path);
         }
     }
     unsafe {
         println!("{:?}", MATCHED_FILES);
-        println!("Mine: {}", size);
     };
 }
 
 fn begin_status() {}
-
-fn search_size(paths: PathBuf) -> u64 {
-    fn ss(dir: fs::ReadDir) -> u64 {
-        let mut size: u64 = 0;
-        for path in dir {
-            let p: PathBuf = path.as_ref().unwrap().path();
-            match fs::read_dir(&p) {
-                Ok(maybe_dir) => size += ss(maybe_dir),
-                Err(_) => size += fs::metadata(p).unwrap().len(),
-            }
-        }
-        size
-    }
-    match fs::read_dir(&paths) {
-        Ok(dir) => ss(dir),
-        Err(_) => fs::metadata(paths).unwrap().len(),
-    }
-}
-
-// From: https://gitlab.com/users/Boiethios/projects | https://stackoverflow.com/questions/60041710/how-to-check-directory-size
-fn dir_size(path: impl Into<PathBuf>) -> io::Result<u64> {
-    fn ds(mut dir: fs::ReadDir) -> io::Result<u64> {
-        dir.try_fold(0, |acc, file| {
-            let file = file?;
-            let size = match file.metadata()? {
-                data if data.is_dir() => ds(fs::read_dir(file.path())?)?,
-                data => data.len(),
-            };
-            Ok(acc + size)
-        })
-    }
-    ds(fs::read_dir(path.into())?)
-}
