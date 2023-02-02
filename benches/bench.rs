6e8aa60cd8928f556c7ec45786a8dfd522ddd6c9
Timothy Cronin <tcroniniv@gmail.com>
2023-02-02 17:14:31 -0500

    add first bench test

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,3 +1,42 @@
+#![allow(unused)] // fix unused warnings printing on *cargo test*
+
+use std::process::Command;
+use std::time::Instant;
+#[path = "../src/main.rs"]
+mod main;
+use crate::main::MATCHED_FILES;
+use crate::main::SEARCHED_SIZE;
+use crate::main::TOTAL_SIZE;
+#[path = "../src/search_files.rs"]
+mod search_files;
+use crate::search_files::loop_files;
+#[path = "../src/parse_args.rs"]
+mod parse_args;
+use crate::parse_args::parse_args;
+
 fn main() {
+    // to print the number of files matched grep -l -r this .doc | wc -l
     println!("grep");
+    grep_personal_website_node_modules();
+}
+
+// bench against grep on large node modules
+fn grep_personal_website_node_modules() {
+    let time = Instant::now();
+    let (path, target) = parse_args(vec![
+        "_".to_string(),
+        "tests/personal_website_node_modules".to_string(),
+        "a".to_string(),
+    ]);
+
+    loop_files(&target, std::fs::read_dir(path).unwrap());
+    println!("Time for bs: {:?}", time.elapsed());
+    let time = Instant::now();
+    let mut grep_cmd = Command::new("grep")
+        .args(["-l", "-r", "a", "tests/personal_website_node_modules"])
+        .output()
+        .expect("grep")
+        .stdout;
+    // i think these are printing out line numbers of located files
+    println!("Grep Time {:?}", time.elapsed());
 }
