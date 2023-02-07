f9515cc7a25b35caccc13044892d971d2d59e15d
Timothy Cronin <tcroniniv@gmail.com>
2023-02-07 16:00:34 -0500

    rewrite flag code

--- src/parse_args.rs
+++ src/parse_args.rs
@@ -1,35 +1,44 @@
 use std::path::PathBuf;
 
 pub fn parse_args(args: Vec<String>) -> (PathBuf, String, bool) {
-    if args.len() == 1 {
-        panic!("No directory or file given");
-    }
-    if args.len() == 2 {
-        panic!("Did not give a target phrase or word");
+    if args.len() < 3 {
+        panic!("Either not given a search location or not given a target phrase");
     }
 
     let first: &str = args.get(1).unwrap();
 
     // if its not a path then check if its a flag
     let mut calc_size: bool = true;
-    let path: &str;
-    let target: String;
+    let mut path: Option<&str> = None;
+    let mut target: Option<String> = None;
     if !std::path::Path::new(first).exists() {
-        if first == "-n" {
-            calc_size = false;
-            if args.len() == 4 {
-                path = args.get(2).unwrap();
-                target = args.get(3).unwrap().replace("\\n", "\n").into();
+        // loop through all the flags
+        for arg in args.iter().skip(1) {
+            if arg.chars().nth(0).unwrap() == '-' {
+                match arg.chars().nth(1) {
+                    Some('n') => calc_size = false,
+                    Some(c) => panic!("{} is not a valid flag", c),
+                    _ => panic!("No character given for flag"),
+                }
             } else {
-                panic!("Not enough arguments given");
+                if path == None {
+                    path = Some(arg);
+                } else {
+                    target = Some(arg.to_string());
+                }
             }
-        } else {
-            panic!("Location {}, doesn't exist", first);
         }
     } else {
-        path = first;
+        path = Some(first);
         target = args.get(2).unwrap().replace("\\n", "\n").into();
     }
+    if path == None || target == None {
+        panic!("Either not given a search location or not given a target phrase");
+    }
 
-    (PathBuf::from(path), target.to_string(), calc_size)
+    (
+        PathBuf::from(path.unwrap()),
+        target.unwrap().to_string(),
+        calc_size,
+    )
 }
