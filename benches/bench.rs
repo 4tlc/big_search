9da8351ae6f286d986d069d461100d54186bd72a
Timothy Cronin <tcroniniv@gmail.com>
2023-02-06 19:08:33 -0500

    add more benchmarks

--- benches/bench.rs
+++ benches/bench.rs
@@ -16,28 +16,27 @@ use crate::parse_args::parse_args;
 
 fn main() {
     // to print the number of files matched grep -l -r this .doc | wc -l
-    grep_personal_website_node_modules();
+    compare_grep(
+        "benches/to_search/personal_website_node_modules",
+        "Personal Website Node Modules",
+    );
+    compare_grep(
+        "benches/to_search/sorting_visualizer_node_modules",
+        "Sorting Visualizer Node Modules",
+    );
 }
 
 // bench against grep on large node modules
-fn grep_personal_website_node_modules() {
+fn compare_grep(folder: &str, name: &str) {
+    println!("\x1b[04\x1b[47mGrep Comparison on {}\x1b[0m", name);
     let time = Instant::now();
-    let (path, target) = parse_args(vec![
-        "_".to_string(),
-        "benches/to_search/personal_website_node_modules".to_string(),
-        "a".to_string(),
-    ]);
+    let (path, target) = parse_args(vec!["_".to_string(), folder.to_string(), "a".to_string()]);
 
     loop_files(&target, std::fs::read_dir(path).unwrap());
     println!("Time for bs: {:?}", time.elapsed());
     let time = Instant::now();
     let mut grep_cmd = Command::new("grep")
-        .args([
-            "-l",
-            "-r",
-            "a",
-            "benches/to_search/personal_website_node_modules",
-        ])
+        .args(["-l", "-r", "a", folder])
         .output()
         .expect("grep")
         .stdout;
