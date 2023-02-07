5151b413af313f39f127bc75d929fe416eae577a
Timothy Cronin <tcroniniv@gmail.com>
2023-02-07 18:18:52 -0500

    improve the error messages

--- src/parse_args.rs
+++ src/parse_args.rs
@@ -1,8 +1,11 @@
+use std::panic::set_hook;
 use std::path::PathBuf;
 
 pub fn parse_args(args: Vec<String>) -> (PathBuf, String, bool) {
     if args.len() < 3 {
-        panic!("Either not given a search location or not given a target phrase");
+        set_hook(Box::new(|_info| {
+            println!("Error: Either not given a search location or not given a target phrase");
+        }));
     }
 
     let first: &str = args.get(1).unwrap();
@@ -17,8 +20,16 @@ pub fn parse_args(args: Vec<String>) -> (PathBuf, String, bool) {
             if arg.chars().nth(0).unwrap() == '-' {
                 match arg.chars().nth(1) {
                     Some('n') => calc_size = false,
-                    Some(c) => panic!("{} is not a valid flag", c),
-                    _ => panic!("No character given for flag"),
+                    Some(c) => {
+                        set_hook(Box::new(move |_info| {
+                            println!("Error: {} is not a valid flag", c);
+                        }));
+                    }
+                    _ => {
+                        set_hook(Box::new(|_info| {
+                            println!("Error: No flag character given after '-'");
+                        }));
+                    }
                 }
             } else {
                 if path == None {
@@ -33,7 +44,9 @@ pub fn parse_args(args: Vec<String>) -> (PathBuf, String, bool) {
         target = args.get(2).unwrap().replace("\\n", "\n").into();
     }
     if path == None || target == None {
-        panic!("Either not given a search location or not given a target phrase");
+        set_hook(Box::new(|_info| {
+            println!("Error: Either not given a search location or not given a target phrase");
+        }));
     }
 
     (
