22862720d594ca7238dc29f910dca67fed180d8d
Timothy Cronin <tcroniniv@gmail.com>
2023-02-14 14:40:16 -0500

    add bench against golang src

--- benches/bench.rs
+++ benches/bench.rs
@@ -21,24 +21,31 @@ fn main() {
     compare_grep(
         "benches/to_search/personal_website_node_modules",
         "Personal Website Node Modules",
+        "a",
     );
     compare_grep(
         "benches/to_search/sorting_visualizer_node_modules",
         "Sorting Visualizer Node Modules",
+        "a",
+    );
+    compare_grep(
+        "benches/to_search/go_master",
+        "Go Lang Source Code",
+        "this shouldn't exist in any",
     );
 }
 
 // bench against grep on large node modules
-fn compare_grep(folder: &str, name: &str) {
+fn compare_grep(folder: &str, name: &str, target: &str) {
     println!("\x1b[04\x1b[47mGrep Comparison on {}\x1b[0m", name);
     let time = Instant::now();
     let bs_cmd = Command::new("target/debug/bs")
-        .args(["-n", folder, "a"])
+        .args(["-n", folder, target])
         .output();
     println!("Time for bs: {:?}", time.elapsed());
     let time = Instant::now();
     let mut grep_cmd = Command::new("grep")
-        .args(["-l", "-r", "a", folder])
+        .args(["-l", "-r", target, folder])
         .output();
     // i think these are printing out line numbers of located files
     println!("Grep Time {:?}", time.elapsed());
